4a5272b079d8b3102f4835c259e837423b942b0c
zhongy-g <zhongy-g@glodon.com>
2021-12-09 15:59:01 +0800

    ces

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,14 @@
 module.exports = {
   root: true,
-  parserOptions: {
-    parser: 'babel-eslint',
-    sourceType: 'module'
-  },
   env: {
-    browser: true,
-    node: true,
-    es6: true,
+    node: true
   },
-  extends: ['plugin:vue/recommended', 'eslint:recommended'],
-  // add your custom rules here
-  //it is base on https://github.com/vuejs/eslint-config-vue
+  extends: ["plugin:vue/essential", "eslint:recommended", "@vue/prettier"],
+  parserOptions: {
+    parser: "babel-eslint"
+  },
   rules: {
-    'no-console': 'error',
+    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
   }
-}
+};
--- packages/gpmbc-button/lib/gpmbc-button.js
+++ packages/gpmbc-button/lib/gpmbc-button.js
@@ -4,6 +4,6 @@ module.exports = gpmbcButton;
 
 function gpmbcButton() {
     // TODO
-    const a = "1000"
-    console.log(12)
+    const a = "1000";
+    console.log(12);
 }
